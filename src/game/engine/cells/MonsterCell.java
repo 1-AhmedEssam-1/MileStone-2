f2ccb1a84999eea00143f2cf03995c77744cdb37
Mohamed-Ashraf-0-0 <medo.4d10@gmail.com>
2026-04-12 22:13:28 +0200

    Update MonsterCell.java

--- src/game/engine/cells/MonsterCell.java
+++ src/game/engine/cells/MonsterCell.java
@@ -13,5 +13,26 @@ public class MonsterCell extends Cell {
 	public Monster getCellMonster() {
 		return cellMonster;
 	}
+	
+	@Override
+	public void onLand(Monster landingMonster, Monster opponentMonster){
+		
+		if (this.getCellMonster().getRole() == landingMonster.getRole()){
+			landingMonster.executePowerupEffect(opponentMonster);
+			// To check if for free
+			return;
+		}
+		
+		if(this.getMonster().getEnergy() < landingMonster.getEnergy()){
+			int tmp = landingMonster.getEnergy();
+			this.getMonster().setEnergy(tmp);
+			if (landingMonster.isShielded()){
+				landingMonster.setShielded(false);
+			}
+			else{
+				landingMonster.setEnergy(this.getMonster().getEnergy());
+			}
+		}
+	}
 
 }
