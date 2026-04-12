729ee5cd4bd70629f9b5c22f801308397231809d
Mohamed-Ashraf-0-0 <medo.4d10@gmail.com>
2026-04-12 22:11:21 +0200

    Update ContaminationSock.java

--- src/game/engine/cells/ContaminationSock.java
+++ src/game/engine/cells/ContaminationSock.java
@@ -12,8 +12,16 @@ public class ContaminationSock extends TransportCell implements CanisterModifier
 
 	@Override
 	public void modifyCanisterEnergy(Monster monster, int canisterValue) {
+		//int val = Constants.SLIP_PENALTY;
+		monster.alterEnergy(-canisterValue);
+	}
+	
+	@Override
+	public void onLand(Monster landingMonster, Monster opponentMonster){
+		this.transport(landingMonster);
 		int val = Constants.SLIP_PENALTY;
-		monster.alterEnergy(-val);
+		this.modifyCanisterEnergy(landingMonster, val);
+
 	}
 	
 
