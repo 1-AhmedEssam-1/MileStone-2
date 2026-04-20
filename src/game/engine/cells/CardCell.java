76ca628b878307fe82fecec20aa38f34f3debefb
Abdelrady10 <mrmido593@gmail.com>
2026-04-20 23:33:16 +0200

    Add onLand method to CardCell class

--- src/game/engine/cells/CardCell.java
+++ src/game/engine/cells/CardCell.java
@@ -1,11 +1,16 @@
 package game.engine.cells;
 
+import game.engine.Board;
+import game.engine.monsters.Monster;
+
 public class CardCell extends Cell {
 	
 	public CardCell(String name) {
         super(name);
     }
+	public void onLand(Monster landingMonster, Monster opponentMonster){
+		Board.drawCard().performAction(landingMonster, opponentMonster);
+	}
 	
-	// waiting for  static Card drawCard() method in the Board class
    
 }
